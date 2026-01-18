856e687fe9f4e4dcc3d78daa43a2942bf51d03a5
Aquid0 <aquidmatthewla@gmail.com>
2026-01-18 01:53:59 +0000

    Install bcrypt

--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -4,7 +4,7 @@ const urlController = require('../controllers/urlController');
 const { authenticate } = require('../middleware/auth');
 
 router.post('/shorten', authenticate({ required: false }), urlController.shortenUrl);
-router.get('/history', authenticate({ required: true }), urlController.getUserUrls);
+// router.get('/history', authenticate({ required: true }), urlController.getUserUrls);
 router.get('/:code', urlController.redirectUrl);
 
 module.exports = router;
